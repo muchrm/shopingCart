ec103fc72b71f81fa5b4e408895c2cee3d38788a
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2017-09-07 20:03:05 +0700

    fiex home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,46 +11,36 @@ export class HomeComponent implements OnInit {
 
   product: any;
   item: any;
-  cart: any = []
-  //countId = 0;
+  cart: any[] = [];
+  // countId = 0;
   countItem = 0;
   constructor(private shopingCartService: ShopingCartService) {
     this.item = {
       id: 0,
-      name: "",
-      prices: "",
-      image: "",
-      color: "",
+      name: '',
+      prices: '',
+      image: '',
+      color: '',
       total: 0,
       out_of_stock: false
-    }
-    console.log(this.item)
+    };
+    console.log(this.item);
   }
 
   ngOnInit() {
     this.shopingCartService.getProduct().subscribe(res => {
       this.product = res;
       //  console.log(this.product)
-    })
+    });
   }
   addCart(product) {
-
-    if (this.cart.length != 0) {
-      for (let i = 0; i < this.cart.length; i++) {
-        //console.log(product)
-        if (this.cart[i].id != product.id) {
-          this.item = product
-          this.cart.push(this.item)
-        } else {
-          console.log("no")
-        }
-      }
-
+    const index = this.cart.indexOf(product);
+    if (index !== -1) {
+      this.cart[index].quantity++;
     } else {
-      this.item = product
-      this.cart.push(this.item)
+      product.quantity = 1;
+      this.cart.push(product);
     }
-    console.log(this.cart)
   }
 
 
